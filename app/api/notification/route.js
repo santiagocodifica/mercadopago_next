5da75da870a01a7e1a17e4b90e64bbf696d8e415
Santiago <santiagopm36@gmail.com>
2023-11-15 14:35:23 -0300

    third commit

--- app/api/notification/route.js
+++ app/api/notification/route.js
@@ -1,3 +1,7 @@
-export async function POST(){
-  return new Response("hola")
+export async function POST(req){
+
+  const body = req.json()
+  console.log("notification: ", body)
+
+  return new Response(body)
 }
